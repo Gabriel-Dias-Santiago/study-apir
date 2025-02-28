e0b38bbcd4a10eda47e74ce01e5c60d890892c8f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 21:40:28 -0300

    feat: add list

---
+++ src/main/java/com/github/gabriel_dias_santiago/study_apir/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.github.gabriel_dias_santiago.study_apir.service;
+
+import java.util.List;
+
+import com.github.gabriel_dias_santiago.study_apir.model.Product;
+
+public class ProductService {
+    private List<Product> products;
+
+}
